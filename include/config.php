a6abb09e84808468528dcf11064b42ccaa37d6f9
jarahtech@gmail.com <jarahtech@gmail.com>
2024-03-02 15:25:15 +0600

    First Commit -2

---
+++ include/config.php
@@ -0,0 +1,35 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mahabub'] = array ('1'=>'mahabub<|<admin','mahabub>|>eZqgrZWWoZ9lamNmbWdjb3FW');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$data['CAMP-12'] = array ('1'=>'CAMP-12!d8580ebf34d6.sn.mynetname.net','CAMP-12@|@shafi','CAMP-12#|#eHE=','CAMP-12%CAMP-12','CAMP-12^login.net','CAMP-12&QR','CAMP-12*10','CAMP-12(1','CAMP-12)','CAMP-12=10','CAMP-12@!@enable');
+$data['CAMP-13'] = array ('1'=>'CAMP-13!hen08zn4vbw.sn.mynetname.net','CAMP-13@|@shafi','CAMP-13#|#eHE=','CAMP-13%CAMP-13','CAMP-13^login.net','CAMP-13&QR','CAMP-13*10','CAMP-13(1','CAMP-13)','CAMP-13=10','CAMP-13@!@enable');
+$data['CAMP-11'] = array ('1'=>'CAMP-11!hd908btnwvy.sn.mynetname.net','CAMP-11@|@shafi','CAMP-11#|#eHE=','CAMP-11%CAMP-11','CAMP-11^login.net','CAMP-11&QR','CAMP-11*10','CAMP-11(1','CAMP-11)','CAMP-11=10','CAMP-11@!@enable');
+$data['CAMP-10'] = array ('1'=>'CAMP-10!hcj08axcrjp.sn.mynetname.net','CAMP-10@|@shafi','CAMP-10#|#eHE=','CAMP-10%CAMP-10','CAMP-10^login.net','CAMP-10&QR','CAMP-10*10','CAMP-10(1','CAMP-10)','CAMP-10=10','CAMP-10@!@enable');
+
+$data['CAMP-9'] = array ('1'=>'CAMP-9!hen08pn3gcy.sn.mynetname.net','CAMP-9@|@shafi','CAMP-9#|#eHE=','CAMP-9%CAMP-9','CAMP-9^login.net','CAMP-9&QR','CAMP-9*10','CAMP-9(1','CAMP-9)','CAMP-9=10','CAMP-9@!@enable');
